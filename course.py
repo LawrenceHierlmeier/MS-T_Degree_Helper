7dcb1e47b22424aaec611dd4089fbb18b3b6186b
ConnorStarks <crsfvy@mst.edu>
2019-09-19 17:50:28 -0500

    old code basis

---
+++ course.py
@@ -0,0 +1,20 @@
+class Course:
+    def __init__(self, name, hrs, idnum, dep1, dep2=None):
+        self.name = name
+        self.hrs == hrs
+        self.dep1 = dep1
+        self.dep2 = dep2
+        self.idnum = idnum
+        self.prereq = []
+        self.coreq = []
+        self.desc = "No Description Available"
+
+
+class Reqset:
+    def __init__(self):
+        self.options = []
+        self.status = "PLANNED"
+
+
+class program:
+    def
