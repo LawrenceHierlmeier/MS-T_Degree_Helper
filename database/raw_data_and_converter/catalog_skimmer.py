7a6be9d4b907f6e4b9d021760865c0081fadb495
ConnorStarks <crsfvy@mst.edu>
2019-09-20 16:20:07 -0500

    functional data skimmer. Complete Compsci directory in JSON

---
+++ database/raw_data_and_converter/catalog_skimmer.py
@@ -0,0 +1,36 @@
+import re
+import json
+
+#----------------------#
+#Only Change These Variables:
+filestring = "compscicatalogsource.html"
+department = "COMP_SCI"
+#----------------------#
+
+regex = (
+    r"<p class=\"courseblocktitle\" id=\".*?\"><em><strong>.*?;(\d+) +(.*?)</strong></em> \((\D*?) (.*?)\)</p>\n<p class=\"courseblockdesc\">\n(.*?)<br />\n"
+    r"<hr />\n"
+    r"</p>")
+
+
+with open(filestring, 'r') as file:
+    data = file.read()
+
+with open("idkeeper", 'r') as file:
+    global index
+    index = int(file.read())
+
+matches = re.finditer(regex, data, re.MULTILINE | re.DOTALL)
+
+list_of_courses = []
+
+for matchNum, match in enumerate(matches, start=1):
+    index += 1
+    temp = {"name":match.group(2),"coursenum":match.group(1),"internal_id":index,"department":department,"hrs":match.group(4),"type":match.group(3),"description":match.group(5)}
+    list_of_courses.append(temp)
+
+with open("mstdatabase.json", 'w') as file:
+    json.dump(list_of_courses,file, indent=4)
+
+with open("idkeeper", 'w') as file:
+    file.write(str(index))
